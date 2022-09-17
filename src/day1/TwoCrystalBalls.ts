60c5668727246dfa6c06cbea671065c081524c76
Max Zawisa <maxrzaw@umich.edu>
2022-09-17 15:38:36 -0400

    clean up TwoCrystalBalls a bit

--- src/day1/TwoCrystalBalls.ts
+++ src/day1/TwoCrystalBalls.ts
@@ -1,18 +1,22 @@
 export default function two_crystal_balls(breaks: boolean[]): number {
     const jumpLength = Math.floor(Math.sqrt(breaks.length));
     let position: number = 0;
+    let found = false;
 
+    // First Crystal ball
     for (; position < breaks.length; position += jumpLength) {
         if (breaks[position]) {
             break;
         }
     }
 
+    // Use the second ball to find the exact floor.
     position -= jumpLength;
     for (; position < breaks.length; position += 1) {
         if (breaks[position]) {
-            return position;
+            found = true;
+            break;
         }
     }
-    return -1;
+    return found ? position : -1;
 }
