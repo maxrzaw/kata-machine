b5abfe29969e0f5b04277cc4c60cc323af68d929
Max Zawisa <maxrzaw@umich.edu>
2022-10-10 17:31:42 -0400

    Day 18 First Try!!

---
+++ src/day18/CompareBinaryTrees.ts
@@ -0,0 +1,10 @@
+// Depth first search preserves structure
+export default function compare(a: BinaryNode<number> | null, b: BinaryNode<number> | null): boolean {
+    if (a === null && b === null) {
+        return true;
+    } else if (a === null || b === null) {
+        return false;
+    }
+
+    return a.value === b.value && compare(a.left, b.left) && compare(a.right, b.right);
+}
