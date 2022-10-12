fcec200bc72e62720d506330e0fa9f557f076a34
Max Zawisa <maxrzaw@umich.edu>
2022-10-11 22:02:24 -0400

    day 19!!!

---
+++ src/day19/BTInOrder.ts
@@ -0,0 +1,17 @@
+function walk(head: BinaryNode<number> | null, path: number[]): void {
+    if (head === null) {
+        return;
+    }
+
+    walk(head.left, path);
+
+    path.push(head.value);
+
+    walk(head.right, path);
+}
+
+export default function in_order_search(head: BinaryNode<number>): number[] {
+    let path: number[] = [];
+    walk(head, path);
+    return path;
+}
---
+++ src/day19/RingBuffer.ts
@@ -0,0 +1,57 @@
+export default class RingBuffer<T> {
+    public length: number;
+    private data: T[];
+    private head: number;
+    
+    constructor(capacity: number = 5) {
+        this.data = Array(capacity);
+        this.head = 0;
+        this.length = 0;
+    }
+
+    push(item: T): void {
+        this.grow();
+
+        this.data[this.getRingIdx(this.length)] = item;
+        this.length++;
+    }
+
+    get(idx: number): T | undefined {
+        if (idx >= this.length || idx < 0) {
+            return undefined;
+        }
+
+        return this.data[this.getRingIdx(idx)];
+    }
+
+    pop(): T | undefined {
+        if (this.length === 0) {
+            return undefined;
+        }
+
+        const idx = this.head;
+        this.head = (this.head + 1) % this.data.length;
+        this.length--;
+
+        return this.data[idx];
+    }
+
+    private getRingIdx(idx: number): number {
+        return (this.head + idx) % this.data.length;
+    }
+
+    private grow() {
+        if (this.length + 1 < this.data.length) {
+            return;
+        }
+
+        const prev_data = this.data;
+        this.data = Array(prev_data.length * 2);
+
+        for (let i = 0; i < this.length; i++) {
+            this.data[i] = prev_data[this.getRingIdx(i)];
+        }
+
+        this.head = 0;
+    }
+}
