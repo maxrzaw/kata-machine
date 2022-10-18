c32513de5d15be4e3d9b0c919390a4d146d31943
Max Zawisa <maxrzaw@umich.edu>
2022-10-18 13:40:46 -0400

    gg Day 24!

---
+++ src/day24/BTPreOrder.ts
@@ -0,0 +1,15 @@
+function walk(head: BinaryNode<number> | null, path: number[]): void {
+    if (head === null) {
+        return;
+    }
+
+    path.push(head.value);
+    walk(head.left, path);
+    walk(head.right, path);
+}
+
+export default function pre_order_search(head: BinaryNode<number>): number[] {
+    let path: number[] = [];
+    walk(head, path);
+    return path;
+}
