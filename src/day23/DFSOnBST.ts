7386ffc6853aee0ee17bd0073a3759e059f7a1bb
Max Zawisa <maxrzaw@umich.edu>
2022-10-18 01:00:03 -0400

    fixed it :)

--- src/day23/DFSOnBST.ts
+++ src/day23/DFSOnBST.ts
@@ -3,8 +3,8 @@ export default function dfs(head: BinaryNode<number>, needle: number): boolean {
         return true;
     }
 
-    let left: boolean = head.left === null ? false : dfs(head.left, needle);
-    let right: boolean = head.right === null ? false : dfs(head.right, needle);
-
-    return left || right;
+    if (head.value < needle) {
+        return head.right === null ? false : dfs(head.right, needle);
+    }
+    return head.left === null ? false : dfs(head.left, needle);
 }
