21b4abfdae1cdde58b4a38080898b7d5d0a5c2a5
Max Zawisa <maxrzaw@umich.edu>
2025-11-09 15:53:41 -0500

    some upgrades

---
+++ jest.config.cjs
@@ -0,0 +1,3 @@
+const config = require("./.jest.config.json");
+
+module.exports = config;
--- jest.config.ts
+++
@@ -1,4 +0,0 @@
-// @ts-ignore
-import jest_config from "./.jest.config.json";
-
-export default jest_config;
