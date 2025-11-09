ec07748c0e37c84409de1d14b864592441039741
Max Zawisa <maxrzaw@umich.edu>
2025-11-09 14:23:31 -0500

    Add more Trie tests

--- src/__tests__/Trie.ts
+++ src/__tests__/Trie.ts
@@ -1,23 +1,18 @@
 import Trie from "@code/Trie";
 
-test("Trie", function() {
+test("Trie", function () {
     const trie = new Trie();
     trie.insert("foo");
     trie.insert("fool");
     trie.insert("foolish");
     trie.insert("bar");
 
-    expect(trie.find("fo").sort()).toEqual([
-        "foo",
-        "fool",
-        "foolish",
-    ]);
+    expect(trie.find("fo").sort()).toEqual(["foo", "fool", "foolish"]);
+
+    expect(trie.find("foo").sort()).toEqual(["foo", "fool", "foolish"]);
+    expect(trie.find("fooo").sort()).toEqual([]);
 
     trie.delete("fool");
 
-    expect(trie.find("fo").sort()).toEqual([
-        "foo",
-        "foolish",
-    ]);
+    expect(trie.find("fo").sort()).toEqual(["foo", "foolish"]);
 });
-
